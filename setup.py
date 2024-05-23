2ea6112639b32cca7949938c8a98fff3775fb543
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-23 15:18:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
 setup(
     name='External-Plugins',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'google_play_scraper',
-        'requests',
-    ],
+    install_requires=read_requirements(),
 )
