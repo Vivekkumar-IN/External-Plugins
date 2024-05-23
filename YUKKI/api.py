d47a04c808bfbf57d9584f32d8591c485914a9c8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-23 13:23:49 +0530

    Update api.py

--- YUKKI/api.py
+++ YUKKI/api.py
@@ -6,7 +6,8 @@ class api:
         pass
 
     def quote(self):
-        a = requests.get("https://api.quotable.io/random")
+        qut = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x61\x70\x69\x2e\x71\x75\x6f\x74\x61\x62\x6c\x65\x2e\x69\x6f\x2f\x72\x61\x6e\x64\x6f\x6d"
+        a = requests.get(qut)
         b = a.json()
         quote = b['content']
         author = b['author']
