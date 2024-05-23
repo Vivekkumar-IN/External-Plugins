fd1c9374a370dd30e32b8aecb230fdb141605781
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-23 10:59:30 +0530

    Create app.py

---
+++ YUKKI/app.py
@@ -0,0 +1,15 @@
+from google_play_scraper import search
+
+class App:
+    def __init__(self):
+        pass
+
+    def search_apps(self, query, lang='en', country='us', n_hits=3):
+        result = search(
+            query,
+            lang=lang,
+            country=country,
+            n_hits=n_hits
+        )
+        return result
+      
