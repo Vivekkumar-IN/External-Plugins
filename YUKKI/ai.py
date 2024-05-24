74d27f0c6a4efa43f2aadb81e0590a8459006efc
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-24 21:52:43 +0530

    Update ai.py

--- YUKKI/ai.py
+++ YUKKI/ai.py
@@ -22,6 +22,7 @@ SOFTWARE.
 '''
 import requests
 from MukeshAPI import api as MAPI
+import aiohttp
 
 class Ai:
     def __init__(self):
@@ -51,4 +52,20 @@ class Mukesh:
         results = a['results']
         return {"results":results,"join": "@ZeroXCoderZChat", "success": True}
 
+    async def paste(text):
+        BASE = "https://batbin.me/"
+        async def post(url: str, *args, **kwargs):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, *args, **kwargs) as resp:
+                try:
+                    data = await resp.json()
+                except Exception:
+                    data = await resp.text()
+            return data
+
+        resp = await post(f"{BASE}api/v2/paste", data=text)
+        if not resp["success"]:
+            return
+        link = BASE + resp["message"]
+        return {"link":link,"join": "@ZeroXCoderZChat", "success": True}
         
