64929f4a9a2c1c65b5c0a3b6402e663f78697472
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-25 07:07:50 +0530

    Update ai.py

--- YUKKI/ai.py
+++ YUKKI/ai.py
@@ -20,33 +20,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
-import requests
-from MukeshAPI import api as MAPI
-
-class Ai:
-    def __init__(self):
-        pass
-
-    def chatgpt(self, query):
-        response = requests.get(
-         f"https://chatgpt.apinepdev.workers.dev/?question={query}"
-        )
-        # this api belongs to nep coders of @DEVSNP
-        if response.status_code == 200:
-            result = response.json()["answer"]
-            return {"results":result,"join": "@ZeroXCoderZChat", "success": True}
-
-    def imagine(self, query):
-        result = f"https://aiimage.hellonepdevs.workers.dev/?prompt={query}"
-        return result
-
-
-# i stored this apis or function from MukeshApi libraray and its develeoper is @Itz_legendCoder
-class Mukesh:
-    def __init__(self):
-        pass
-
-    def gemini(self, query):
-        a = MAPI.gemini(query)
-        results = a['results']
-        return {"results":results,"join": "@ZeroXCoderZChat", "success": True}
