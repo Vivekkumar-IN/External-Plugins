e0514573cc4d722082d504752b77dc900cfeb034
VivekKumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-29 03:05:16 +0000

    Pylint: Auto Fixes

--- TheApi/api.py
+++ TheApi/api.py
@@ -6,7 +6,6 @@ import nltk
 from io import BytesIO
 from bs4 import BeautifulSoup
 from nltk.corpus import words
-from random_word import RandomWords
 from PIL import Image, ImageDraw, ImageFont
 from telegraph import upload_file, Telegraph
 from .errors import InvalidAmountError
@@ -14,9 +13,9 @@ from .functions import MORSE_CODE_DICT
 
 telegraph = Telegraph()
 
-telegraph.create_account(short_name='RAM SIYA RAM')
+telegraph.create_account(short_name="RAM SIYA RAM")
 
-nltk.download('words')
+nltk.download("words")
 
 
 class Myapi:
@@ -27,14 +26,13 @@ class Myapi:
         qut = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x61\x70\x69\x2e\x71\x75\x6f\x74\x61\x62\x6c\x65\x2e\x69\x6f\x2f\x72\x61\x6e\x64\x6f\x6d"
         a = requests.get(qut)
         b = a.json()
-        quote = b['content']
-        author = b['author']
+        quote = b["content"]
+        author = b["author"]
         return {"quote": quote, "author": author, "join": "@vk_zone"}
- 
+
     def randomword(self):
         word_list = words.words()
         return random.choice(word_list)
-
 
     def write(self, text):
         tryimg = "https://graph.org/file/1f8d00177ac2429b101b9.jpg"
@@ -56,12 +54,12 @@ class Myapi:
                     lines.append(line)
                 else:
                     k = len(line) // 55
-                    lines.extend(line[((z - 1) * 55) : (z * 55)] for z in range(1, k + 2))
-    
-    
-    
+                    lines.extend(
+                        line[((z - 1) * 55) : (z * 55)] for z in range(1, k + 2)
+                    )
+
         umm = lines[:25]
-        
+
         line_height = font.getbbox("hg")[3]
         linespacing = 41
         for line in umm:
@@ -81,11 +79,11 @@ class Myapi:
 
         response = requests.post(url, json={"code": code})
         image = BytesIO(response.content)
-    
+
         a = self.randomword()
         image.name = f"{a}.png"
 
-        with open(image.name, 'wb') as f:
+        with open(image.name, "wb") as f:
             f.write(image.getbuffer())
 
         if os.path.exists(image.name):
@@ -96,12 +94,12 @@ class Myapi:
 
     def chatgpt(self, query):
         response = requests.get(
-         f"https://chatgpt.apinepdev.workers.dev/?question={query}"
+            f"https://chatgpt.apinepdev.workers.dev/?question={query}"
         )
         # this api belongs to nep coders of @DEVSNP
         if response.status_code == 200:
             result = response.json()["answer"]
-            return {"results":result,"join": "@vk_zone", "success": True}
+            return {"results": result, "join": "@vk_zone", "success": True}
 
     def imagine(self, query):
         image_url = f"https://aiimage.hellonepdevs.workers.dev/?prompt={query}"
@@ -117,52 +115,58 @@ class Myapi:
         return f"https://telegra.ph{uploaded_file[0]}"
 
     def telegraph(self, title, query):
-        '''
+        """
         This project includes code from the python273's telegraph library, which is licensed under the MIT License.
 
         MIT License
         Copyright (c) 2018 python273
 
-        '''
-        response = telegraph.create_page(
-        title,
-        html_content=query
-        )
-        url = response['url']
-        return {"results":url,"join": "@vk_zone", "success": True}
-
+        """
+        response = telegraph.create_page(title, html_content=query)
+        url = response["url"]
+        return {"results": url, "join": "@vk_zone", "success": True}
 
     def get_advice(self):
         try:
-            results = requests.get("https://api.adviceslip.com/advice").json()['slip']['advice']
-            return {"results":results,"join": "@TheTeamVivek", "success": True}
+            results = requests.get("https://api.adviceslip.com/advice").json()["slip"][
+                "advice"
+            ]
+            return {"results": results, "join": "@TheTeamVivek", "success": True}
         except requests.exceptions.RequestException as e:
             return e
-
 
     def get_jokes(self, amount=1):
         if amount > 10 or amount < 1:
             raise InvalidAmountError(amount)
-        response = requests.get(f"https://v2.jokeapi.dev/joke/Any?type=single&amount={amount}")
+        response = requests.get(
+            f"https://v2.jokeapi.dev/joke/Any?type=single&amount={amount}"
+        )
         jokes_data = response.json()
-      
+
         if amount == 1:
-            jokes = {"jokes": {"joke": jokes_data['joke']}}
+            jokes = {"jokes": {"joke": jokes_data["joke"]}}
         else:
-            jokes = {"jokes": {f"joke{i+1}": joke['joke'] for i, joke in enumerate(jokes_data['jokes'])}}
-    
+            jokes = {
+                "jokes": {
+                    f"joke{i+1}": joke["joke"]
+                    for i, joke in enumerate(jokes_data["jokes"])
+                }
+            }
+
         return json.dumps(jokes)
-        
+
     def get_hindi_jokes(self):
         JOKE_API_ENDPOINT = "https://hindi-jokes-api.onrender.com/jokes?api_key=93eeccc9d663115eba73839b3cd9"
         response = requests.get(JOKE_API_ENDPOINT).json()
-        if response['status']:    
-            results = response['jokeContent']
+        if response["status"]:
+            results = response["jokeContent"]
             return results
 
     def get_uselessfact(self):
-        results = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random").json()['text']
-        return {"results": results,"join": "@TheTeamVivek", "success": True}
+        results = requests.get(
+            "https://uselessfacts.jsph.pl/api/v2/facts/random"
+        ).json()["text"]
+        return {"results": results, "join": "@TheTeamVivek", "success": True}
 
     def gen_hashtag(self, text):
         url = "https://all-hashtag.com/library/contents/ajax_generator.php"
@@ -172,70 +176,76 @@ class Myapi:
             "filter": "top",
         }
         response = requests.post(url, data=data)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        hashtags_div = soup.find('div', id='copy-hashtags')
+        soup = BeautifulSoup(response.text, "html.parser")
+        hashtags_div = soup.find("div", id="copy-hashtags")
         hashtags = hashtags_div.text.strip() if hashtags_div else ""
-        similar_hashtags_div = soup.find('div', id='copy-hashtags-similar')
-        similar_hashtags = similar_hashtags_div.text.strip() if similar_hashtags_div else ""
-        return f"Hashtags:\n{hashtags}\n\n Similar hashtags:\n{similar_hashtags}" 
+        similar_hashtags_div = soup.find("div", id="copy-hashtags-similar")
+        similar_hashtags = (
+            similar_hashtags_div.text.strip() if similar_hashtags_div else ""
+        )
+        return f"Hashtags:\n{hashtags}\n\n Similar hashtags:\n{similar_hashtags}"
 
     def morse_code(self, txt):
-        MORSE_CODE_DICT_REVERSED = {value: key for key, value in MORSE_CODE_DICT.items()}
+        MORSE_CODE_DICT_REVERSED = {
+            value: key for key, value in MORSE_CODE_DICT.items()
+        }
         is_morse = all(char in ".- /" for char in txt)
-   
+
         if is_morse:
-            decoded_message = ''
-            words = txt.split(' / ')
+            decoded_message = ""
+            words = txt.split(" / ")
             for word in words:
                 for morse_char in word.split():
                     if morse_char in MORSE_CODE_DICT_REVERSED:
                         decoded_message += MORSE_CODE_DICT_REVERSED[morse_char]
                     else:
                         return f"Error: Morse code '{morse_char}' not recognized."
-                decoded_message += ' '
+                decoded_message += " "
             return decoded_message.strip()
         else:
-            encoded_message = ''
+            encoded_message = ""
             for char in txt.upper():
                 if char in MORSE_CODE_DICT:
-                    encoded_message += MORSE_CODE_DICT[char] + ' '
+                    encoded_message += MORSE_CODE_DICT[char] + " "
                 else:
                     return f"Error: Character '{char}' cannot be encoded in Morse code."
             return encoded_message.strip()
 
     def wikipedia(self, query):
         search_url = "https://en.wikipedia.org/w/api.php"
-    
+
         params = {
             "action": "query",
             "list": "search",
             "srsearch": query,
-            "format": "json"
+            "format": "json",
         }
-    
+
         response = requests.get(search_url, params=params)
-    
+
         if response.status_code == 200:
             data = response.json()
             search_results = data.get("query", {}).get("search", [])
-        
+
             if search_results:
                 top_result = search_results[0]
                 page_id = top_result["pageid"]
                 summary_url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&exintro&explaintext&piprop=thumbnail&pithumbsize=500&format=json&pageids={page_id}"
                 summary_response = requests.get(summary_url)
-            
+
                 if summary_response.status_code == 200:
                     summary_data = summary_response.json()
                     pages = summary_data.get("query", {}).get("pages", {})
                     page_info = pages.get(str(page_id), {})
-                    image_url = page_info.get("thumbnail", {}).get("source", "No image available")
-                
+                    image_url = page_info.get("thumbnail", {}).get(
+                        "source", "No image available"
+                    )
+
                     return {
                         "title": top_result["title"],
                         "summary": page_info.get("extract", "No summary available."),
                         "url": f"https://en.wikipedia.org/?curid={page_id}",
-                        "image_url": image_url
+                        "image_url": image_url,
                     }
                 else:
                     return {"error": "Failed to fetch the page summary"}
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
+
 def read_requirements():
-    with open('requirements.txt') as f:
+    with open("requirements.txt") as f:
         return f.read().splitlines()
 
 
 setup(
-    name='TheApi',
-    version='0.9',
+    name="TheApi",
+    version="0.9",
     packages=find_packages(),
     install_requires=read_requirements(),
 )
